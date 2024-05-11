a0d8171926cfb1be733f6027fa83e91c1d5c6822
Murad Gojayev <muradqocayevi20@gmail.com>
2024-05-11 14:47:04 +0400

    updated

---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Header } from "@/components/auth/header";
+import { Social } from "@/components/auth/social";
+import { BackButton } from "@/components/auth/back-button";
+
+interface ICardWrapperProps {
+  children: ReactNode;
+  headerLabel: string;
+  backButtonLabel: string;
+  backButtonHref: string;
+  showSocial?: boolean;
+}
+
+export const CardWrapper = ({
+  backButtonHref,
+  backButtonLabel,
+  children,
+  headerLabel,
+  showSocial,
+}: ICardWrapperProps) => {
+  return (
+    <Card className="w-[400px] shadow-md">
+      <CardHeader>
+        <Header label={headerLabel} />
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+      {showSocial && (
+        <CardFooter>
+          <Social />
+        </CardFooter>
+      )}
+      <CardFooter>
+        <BackButton label={backButtonLabel} href={backButtonHref} />
+      </CardFooter>
+    </Card>
+  );
+};
